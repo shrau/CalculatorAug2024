69a01d5e6677ca2527d6c2edac3e3f9cc85e1840
Shravani <shramore58@gmail.com>
2024-08-29 22:08:23 +0530

    First Commit

---
+++ build/lib/simplecalculator24/calculations.py
@@ -0,0 +1,12 @@
+def addition(num1,num2):
+    return num1+num2
+
+
+def subtraction(num1,num2):
+    return num1-num2
+
+def multiplication(num1,num2):
+    return num1*num2
+
+def division(num1,num2):
+    return num1/num2
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="simplecalculator24",
+    version="0.0.1",
+    author="Shravani Mpore",
+    author_email="shramore58@gmail.com",
+    description="Package to get calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires= '>=3.7',
+)
